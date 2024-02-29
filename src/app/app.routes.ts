3589079abbff4ff7e8df41cb3f7733a87db11583
Amos Adebisi <adebisiayodeji20@gmail.com>
2024-02-29 09:23:29 +0100

    modifications for routing done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
 import { AlertComponent } from './alert/alert.component';
 import { MonitorComponent } from './monitor/monitor.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
-    {path:"", component:MainDashboardComponent},
+    {path: '', component: HomeComponent },
+    {path:'login', component: LoginComponent },
+    {path:'dashboard', component: MainDashboardComponent},
     {path:"alert", component:AlertComponent},
     {path:"monitor", component:MonitorComponent}
 ];
