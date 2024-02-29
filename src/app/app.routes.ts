e0017af399021c5ebcacf9f6789190f6a2950824
Amos Adebisi <adebisiayodeji20@gmail.com>
2024-02-29 08:41:40 +0100

    updated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { AlertComponent } from './alert/alert.component';
+import { MonitorComponent } from './monitor/monitor.component';
 
 export const routes: Routes = [
-    { 'path': '', component: HomeComponent },
-    { 'path':'login', component: LoginComponent },
+    {path:"", component:MainDashboardComponent},
+    {path:"alert", component:AlertComponent},
+    {path:"monitor", component:MonitorComponent}
 ];
