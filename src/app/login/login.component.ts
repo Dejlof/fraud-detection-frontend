3a9bb63e3435116e8d5ab0117ce6ebdded8b6573
ABQ <obeitoraq20@gmail.com>
2024-03-01 13:36:57 +0100

    implemented transaction page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink , Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  
+  hidePassword: boolean = true;
 
   constructor(private router:Router){
 
   }
+
+  togglePassword(){
+      this.hidePassword = !this.hidePassword;
+  }
+
   gotoDashboard(){
     this.router.navigate(['/dashboard'])
   };
