c4e89bfe8c59e4a8a4e8a81313fa32034617bcaa
ABQ <obeitoraq20@gmail.com>
2024-03-02 11:36:33 +0100

    added transaction success page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { MonitorComponent } from './monitor/monitor.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { TransactionComponent } from './transaction/transaction.component';
+import { TransactSuccessComponent } from './transact-success/transact-success.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent },
@@ -12,5 +13,6 @@ export const routes: Routes = [
     {path:'dashboard', component: MainDashboardComponent},
     {path:"alert", component:AlertComponent},
     {path:"monitor", component:MonitorComponent},
-    {path:"transaction", component:TransactionComponent}
+    {path:"transaction", component:TransactionComponent},
+    {path:"transaction-successful", component:TransactSuccessComponent}
 ];
---
+++ src/app/transact-failed/transact-failed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transact-failed',
+  standalone: true,
+  imports: [],
+  templateUrl: './transact-failed.component.html',
+  styleUrl: './transact-failed.component.css'
+})
+export class TransactFailedComponent {
+
+}
