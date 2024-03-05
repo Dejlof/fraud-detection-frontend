7602bfd46954dfb6cd7f74a455dd959055936031
ABQ <obeitoraq20@gmail.com>
2024-03-05 07:47:12 +0100

    login integrated with api

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { RouterLink , Router } from '@angular/router';
 import { Admin } from '../models/admin';
@@ -11,44 +11,60 @@ import { error } from 'console';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule],
+  imports: [RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 
-export class LoginComponent {
+export class LoginComponent{
+  adminLogin: FormGroup;
   loginObj: Admin = {Username: '', Password: ''};
   hidePassword: boolean = true;
 
-  constructor(private loginService:LoginServiceService){
+  constructor(private router:Router, private loginService:LoginServiceService){
     this.loginObj = this.loginObj;
+    this.adminLogin = new FormGroup({
+      username: new FormControl("",[Validators.required]),
+      password: new FormControl("",[Validators.required]),
+    });
   }
 
-  onLogin(){
-    this.loginService.login(this.loginObj).subscribe(
-      (response:any)=>{
-        if (this.isLoginSuccessful(response)) {
-          alert("Login Success");
-        } else {
-          alert("Login Failed");
+  onSubmit(){
+    if(this.adminLogin.valid){
+      this.loginObj.Username = this.adminLogin.controls['username'].value;
+      this.loginObj.Password = this.adminLogin.controls['password'].value;
+      this.loginService.login(this.loginObj).subscribe(
+        (response: any)=>{
+          if (this.isLoginSuccessful(response)) {
+            this.gotoDashboard();
+          } 
+          else {
+            console.log('Invalid username or password');
+            alert('Invalid username or password');
+          }
+        },
+        (error:any)=>{
+          console.log('Error occurred:', error);
+          alert("An error occurred during login");
         }
-      },
-      (error:any)=>{
-        console.log('Error occurred:', error);
-        alert("An error occurred during login");
-      }
-    )
+      )
+    } 
+    else {
+      console.log('Please fill all the fields');
+      alert('Please fill all the fields');
+    }
   }
 
   private isLoginSuccessful(response: any): boolean {
-    return response && response.Username === 'super_admin' && response.Password === 'admin@123';
+    return this.loginObj.Username === 'super_admin' && this.loginObj.Password === 'admin@123';
   }
 
   togglePassword(){
       this.hidePassword = !this.hidePassword;
   }
 
-  // gotoDashboard(){
-  //   this.router.navigate(['/dashboard'])
-  // };
+  gotoDashboard(){
+    this.router.navigate(['/dashboard'])
+  };
+  
 }
--- src/app/service/login/login-service.service.ts
+++ src/app/service/login/login-service.service.ts
@@ -2,6 +2,7 @@ import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Admin } from '../../models/admin';
 import { Observable } from 'rxjs';
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
