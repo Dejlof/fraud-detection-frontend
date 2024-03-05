8e0804a7c688ee38aa1513e734852e4641b9e12a
Amos Adebisi <adebisiayodeji20@gmail.com>
2024-03-05 19:15:52 +0100

    bug fixes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink , Router } from '@angular/router';
@@ -7,7 +6,7 @@ import { RouterLink , Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink, FormsModule, HttpClientModule],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -15,40 +14,15 @@ export class LoginComponent {
   
   hidePassword: boolean = true;
 
-  
+  constructor(private router:Router){
+
+  }
 
   togglePassword(){
       this.hidePassword = !this.hidePassword;
   }
 
-  loginObj:Login;
-
-  constructor(private http:HttpClient, private router:Router){
-    this.loginObj= new Login;
+  gotoDashboard(){
+    this.router.navigate(['/dashboard'])
   };
-  
-onLogin(){
-  debugger;
-  this.http.post('https://freeapi.gerasim.in/api/User/Login', this.loginObj).subscribe((res:any)=>{
-    if(res.result){
-      console.log(res.result);
-      alert("Login Sucess")
-      this.router.navigateByUrl("/dashboard");
-    }else{
-      alert(res.message)
-    }
-  })
-}
-
-}
-
-export class Login {
-    
-  email: string;
- passwordHash: string;
- constructor(){
-  this.email ="";
-  this.passwordHash ="";
- }
- 
 }
